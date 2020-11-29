5ee03d6f49f0ba8d18d36c2c2f04c0317212b095
Abdulrahman Al Asfar <61043093+abdelrahman-elasfar@users.noreply.github.com>
2020-11-29 16:31:29 +0200

    camera controller updated

--- inc/components/flyCameraController.hpp
+++ inc/components/flyCameraController.hpp
@@ -5,8 +5,7 @@
 #include <glm/gtc/constants.hpp>
 #include <glm/trigonometric.hpp>
 #include <glm/gtx/fast_trigonometry.hpp>
-
-#include <components/transform.hpp>
+#include <components/camera.hpp>
 #include <components/component.h>
 #include <application.hpp>
 
@@ -14,7 +13,7 @@
     class FlyCameraController : public Component {
     private:
         GraphicsProject::Application* app;
-        Transform* transform;
+        Camera* camera;
 
         float yaw, pitch;
         glm::vec3 position;
@@ -30,15 +29,14 @@
     FlyCameraController (std::weak_ptr<Entity> entity,GraphicsProject::Application* application) :Component(entity)
 	{
 		type = CAMERACONTROLLER;
-        Transform transform = entity->getComp<TRANSFORM>();  //gets transform component of camera entity
+        //Transform* transform = entity->getComp<TRANSFORM>();  //gets transform component of camera entity
 
         this->app = application;
-            this->transform = transform;
             yaw_sensitivity = pitch_sensitivity = 0.01f;
             position_sensitivity = {3.0f, 3.0f, 3.0f};
             fov_sensitivity = glm::pi<float>()/10;
 
-            position = transform->getPosition();
+            position = camera->getEyePosition();
             auto direction = camera->getDirection();
             yaw = glm::atan(-direction.z, direction.x);
             float base_length = glm::sqrt(direction.x * direction.x + direction.z * direction.z);
