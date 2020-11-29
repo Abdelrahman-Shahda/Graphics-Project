9dda693d7dd4a5d9596519593b8ef779e3eaf0fd
shaimaa_osama <shaimaaosama1999@yahoo.com>
2020-11-29 13:03:54 +0200

    transform and game_state_management

---
+++ inc/game_states/game_state.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Hania on 11/29/2020.
+//
+
+#ifndef GRAPHICSPROJECT_GAME_STATE_HPP
+#define GRAPHICSPROJECT_GAME_STATE_HPP
+class game_state {
+public:
+    virtual void onEnter(){}
+    virtual void onDraw1(){}
+    virtual void onExit(){}
+};
+
+#endif //GRAPHICSPROJECT_GAME_STATE_HPP
---
+++ src/components/transform/transform_component.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Hania on 11/29/2020.
+//
+
