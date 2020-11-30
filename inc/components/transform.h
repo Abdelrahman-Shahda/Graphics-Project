37aed6557c6295fe5dc2948271ea9d766f643fe0
Abdulrahman Al Asfar <61043093+abdelrahman-elasfar@users.noreply.github.com>
2020-11-30 08:54:54 +0200

    Transform inherited from Component

--- inc/components/transform.h
+++ inc/components/transform.h
@@ -2,19 +2,22 @@
 //
 // Created by Shaimaa on 11/29/2020.
 // Naming Edited by Asfar 11/30/2020.
+// Inherited from Component by Asfar 11/30/2020
 //
 
 #ifndef GRAPHICSPROJECT_TRANSFORM_COMPONENT_HPP
 #define GRAPHICSPROJECT_TRANSFORM_COMPONENT_HPP
-
 #include <glm/glm.hpp>
-class Transform{
+#include <components/component.h>
+
+class Transform : public Component{
 private:
     glm::mat4 position;
     glm::mat4 Rotation;
     glm::mat4 scale;
     Transform* parent;
 public:
+    Transform (std::weak_ptr<Entity> entity:Component(entity));
     void set_position(glm::vec3 v);
     void set_rotation(glm::vec3 v);
     void set_scale(glm::vec3 v);
--- src/components/transform.cpp
+++ src/components/transform.cpp
@@ -4,7 +4,8 @@
 //
 #include <glm/gtx/euler_angles.hpp>
 #include <components/transform.h>
-void Transform:set_parent(transform_component *transformComponent) {parent=transformComponent;}
+Transform::Transform(std::weak_ptr<Entity> entity:Component(entity)) {type = TRANSFORM;}
+void Transform:set_parent(Transform* transformComponent) {parent=transformComponent;}
 void Transfrom::set_position(glm::vec3 v) {position= glm::translate(glm::mat4(1.0f),v);}
 void Transform::set_scale(glm::vec3 v) {scale=glm::scale(glm::mat4(1.0f),v);}
 void Transform::set_rotation(glm::vec3 v) {Rotation=glm::yawPitchRoll(v.y, v.x, v.z);}
