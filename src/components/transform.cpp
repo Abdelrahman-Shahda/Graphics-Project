3d150d66d5d3aa5af6e8fb5ffad6e1534ec91615
Abdulrahman Al Asfar <61043093+abdelrahman-elasfar@users.noreply.github.com>
2020-11-30 10:52:14 +0200

    Update transform.cpp

--- src/components/transform.cpp
+++ src/components/transform.cpp
@@ -10,5 +10,5 @@ void Transfrom::set_position(glm::vec3 v) {position= glm::translate(glm::mat4(1.
 void Transform::set_scale(glm::vec3 v) {scale=glm::scale(glm::mat4(1.0f),v);}
 void Transform::set_rotation(glm::vec3 v) {Rotation=glm::yawPitchRoll(v.y, v.x, v.z);}
 Transform * Transform::get_parent(){return parent;}
-glm::mat4 Transform::get_transform() {return position*scale*Rotation; }
+glm::mat4 Transform::get_transform() {return position*scale*Rotation;}
 
