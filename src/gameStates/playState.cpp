c7b19eaa8cab98346c4ab8786d25e48496ea6edb
Abdelrahman-Shahda <abdelrahmanshahda@yahoo.com>
2021-01-18 01:53:17 +0200

    Adjusted playState.cpp

--- src/gameStates/playState.cpp
+++ src/gameStates/playState.cpp
@@ -348,27 +348,6 @@ std::vector<std::shared_ptr<Entity>> PlayState::getEntitiesWithTag(const std::ve
     return temp;
 }
 
-
-void PlayState::moveelf(double deltaTime) {
-    const int range_from  = 0;
-    const int range_to    = 3;
-
-    std::random_device                  rand_dev;
-    std::mt19937                        generator(rand_dev());
-    std::uniform_int_distribution<int>  distr(range_from, range_to);
-    int x= distr(generator);
-    glm::vec3 position = elf_entity->getComp<Transform>()->get_position()[3];
-    if(x==0) position.z -=  ((float)deltaTime * 20);
-    if(x==1) position.z += ((float)deltaTime * 20);
-    if(x==2) position.x += ((float)deltaTime * 20);
-    if(x==3) position.x -= ((float)deltaTime * 20);
-    if(position.x > -32.5 && position.x < 32.5 && position.z > -22.2 && position.z < 42.8){
-        elf_entity->getComp<Transform>()->set_position(position);
-        elf_entity->getComp<Transform>()->update();
-    }
-
-}
-
 void PlayState::moveSnow(double deltaTime)
 {
     glm::vec3 position;
